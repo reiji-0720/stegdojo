992ff23eaa79ab58165ce7cdc567640707695bb1
REiJi <shino327gh@gmail.com>
2019-09-24 12:15:05 +0900

    anime added

--- Lv1/lv1.js
+++ Lv1/lv1.js
@@ -1,4 +1,16 @@
+
 $(function(){
   
-  $('body').fadeIn(3000);    
-  });
+  $('body').fadeIn(3000);  
+  $('.call').hover  (
+    function(){
+      $('call').css('transition-duration','5s');
+      $('.call').css('filter','drop-shadow(10px 10px 10px rgba(0,0,0,0.6))');
+    },
+    function(){
+
+    }
+  );
+  
+});
+
