dca3c26225a6a9c811da1981889947898e9b40ac
REiJi <shino327gh@gmail.com>
2019-09-24 12:38:10 +0900

    Update lv1.js

--- Lv1/lv1.js
+++ Lv1/lv1.js
@@ -4,12 +4,10 @@ $(function(){
   $('body').fadeIn(3000);  
   $('.call').hover  (
     function(){
-      $('call').css('transition-duration','5s');
+      $('call').css('transition-duration','2s');
       $('.call').css('filter','drop-shadow(10px 10px 10px rgba(0,0,0,0.6))');
-    },
-    function(){
-
     }
+    
   );
   
 });
