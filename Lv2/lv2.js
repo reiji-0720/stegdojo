9b30df0dc531d464dd70e638f150dc72c7de9a18
REiJi <shino327gh@gmail.com>
2019-11-22 10:58:21 +0900

    s

--- Lv2/lv2.js
+++ Lv2/lv2.js
@@ -1,16 +1,10 @@
 
-var count = 0;
+
 
 
 if(count == 0){
     document.getElementById("text-html").onclick = function(){
         document.getElementById("hello,html").innerHTML = "Hello,js!";
-        count += 1;
-    };
-}else{
-    document.getElementById("text-html").onclick = function(){
-        document.getElementById("hello,html").innerHTML = "Hello,HTML";
-    count = 0;
-    
+
     };
 }
