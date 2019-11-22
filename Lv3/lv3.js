5494e925268e8905c039494a60f4e340d4da2219
REiJi <shino327gh@gmail.com>
2019-11-23 00:36:08 +0900

    a

--- Lv3/lv3.js
+++ Lv3/lv3.js
@@ -1,5 +1,4 @@
 function set2fig(num) {
-    // 桁数が1桁だったら先頭に0を加えて2桁に調整する
     var ret;
     if( num < 10 ) { ret = "0" + num; }
     else { ret = num; }
@@ -16,3 +15,4 @@ function set2fig(num) {
  setInterval('showClock2()',1000);
 
 
+　
