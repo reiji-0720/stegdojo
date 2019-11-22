8825121c5142bc099ff33adbb4d5f003ad92e0fd
REiJi <shino327gh@gmail.com>
2019-11-23 03:30:15 +0900

    Update lv3.js

--- Lv3/lv3.js
+++ Lv3/lv3.js
@@ -3,12 +3,12 @@ var button = document.getElementById("button");
 button.addEventListener("click",function(e){
    e.preventDefault();
 
-   var textForm1 = dovument.getElementById("textForm1").value;
+   var textForm1 = document.getElementById("textForm1").value;
    var textForm2 = document.getElementById("textForm2").value;
 
    var sum = parseFloat(textForm1,10) + parseFloat(textForm2,10);
 
-   var resultForm =document.getElementById("resultForm");
+   var resultForm = document.getElementById("resultForm");
    resultForm.value = sum;
 });
 
