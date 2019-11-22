7caa1f5829c492fe89b5ed2752761a939abc9136
REiJi <shino327gh@gmail.com>
2019-11-23 02:06:53 +0900

    Update lv3.js

--- Lv3/lv3.js
+++ Lv3/lv3.js
@@ -1,10 +1,3 @@
-function Add(){
-   document.fm.C.value = document.fm.A.value + document.fm.B.value;
-};
-function Add1(){
-   document.fm1.C.value =eval(document.fm1.A.value) + eval(document.fm1.B.value);
-};
 function Calc_form(){
    document.fm2,Ans.value = eval(document.fm2.Form.value);
 };
-    
