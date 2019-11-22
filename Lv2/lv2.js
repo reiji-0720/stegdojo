b9095d9f72326c04dd5e5ce5347efed990613227
REiJi <shino327gh@gmail.com>
2019-11-22 10:43:27 +0900

    js if文の追加

--- Lv2/lv2.js
+++ Lv2/lv2.js
@@ -1,6 +1,16 @@
 
+var count = 0;
 
 
-document.getElementById("text-html").onclick = function(){
-    document.getElementById("hello,html").innerHTML = "Hello,js!";
-};
+if(count == 0){
+    document.getElementById("text-html").onclick = function(){
+        document.getElementById("hello,html").innerHTML = "Hello,js!";
+        count += 1;
+    };
+}else{
+    document.getElementById("text-html").onclick = function(){
+        document.getElementById("text,html").innerHTML = "Hello,HTML";
+    count = 0s;
+    
+    };
+}
