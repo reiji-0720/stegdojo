36a44ece454c1df581cf083d62c880bf7fb3ad99
REiJi <shino327gh@gmail.com>
2019-11-23 03:19:57 +0900

    s

--- Lv3/lv3.js
+++ Lv3/lv3.js
@@ -1,3 +1,17 @@
+var button = document.getElementById("button");
+
+button.addEventListener("click",function(e){
+   e.preventDefault();
+
+   var textForm1 = dovument.getElementById("textForm1").value;
+   var textForm2 = document.getElementById("textForm2").value;
+
+   var um = textForm1 + textForm2;
+
+   var resultForm =document.getElementById("resultForm");
+   resultForm.value = sum;
+});
+
 function Calc_form(){
-   fm.C.value= eval(fm,A.value)+(fm,B,value);
+   fm.result.value= eval(fm,A.value)+(fm,B,value);
 };
