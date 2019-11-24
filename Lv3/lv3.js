819b934a60f0551879dcdf209c4109b8e8adeff8
REiJi <shino327gh@gmail.com>
2019-11-25 04:26:07 +0900

    Update lv3.js

--- Lv3/lv3.js
+++ Lv3/lv3.js
@@ -4,21 +4,24 @@ var checkString2//桁数チェック
 button.addEventListener("click",function(e){
    e.preventDefault();
 
-   var textForm1 = document.getElementById("textForm1").value;
-   var textForm2 = document.getElementById("textForm2").value;
 
-   var sum = parseFloat(textForm1,10) + parseFloat(textForm2,10);
+  var textForm1 = document.getElementById("textForm1").value;
+  var textForm2 = document.getElementById("textForm2").value;
 
-   var resultForm = document.getElementById("resultForm");
-   document.getElementById("resultForm").innerHTML = sum ;
+  var sum = parseFloat(textForm1,10) + parseFloat(textForm2,10);
+   checkString1 = console.log(String(textForm1).length);
+   checkString2 = console.log(String(textForm2).length);
 
-
-   checkString = console.log(String(textForm1).length);
-
-   if ((checkString >=  10) || (checkString <= 0)){
+   if (((0 <= checkString1) || (checkString1 <= 10))||((0 <= checkString2 )||(checkString <= 10))){
      
-   }else if({
+      
 
+      var resultForm = document.getElementById("resultForm");
+      document.getElementById("resultForm").innerHTML = sum ;
+
+
+   }else{
+    alert("10桁以下しか計算できません")
    }
 
 });
