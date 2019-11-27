d71585b31fba67765f2787f93e3f690a03a1ad71
REiJi <shino327gh@gmail.com>
2019-11-27 16:39:37 +0900

    r

--- Lv8/lv8.js
+++ Lv8/lv8.js
@@ -1,7 +1,56 @@
-$(function() {
-    // 「#hide-text」要素に対するclickイベントを作成してください
-    $('#hide-text').click(function(){
-      $('header').slideUp();
-    })
-    
+$(window).on('load scroll', function (){
+    var box = $('.one');
+    var animated = 'animated';
+    box.each(function(){
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+      if(scrollPos > boxOffset - wh + 300 ){
+        $(this).addClass(animated);
+      }
+    });
+    var box = $('.two');
+    var animated = 'animated';
+    box.each(function(){
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+      if(scrollPos > boxOffset - wh + 300 ){
+        $(this).addClass(animated);
+      }
+    });
+    var box = $('.three');
+    var animated = 'animated';
+    box.each(function(){
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+      if(scrollPos > boxOffset - wh + 300 ){
+        $(this).addClass(animated);
+      }
+    });
+    var box = $('.four');
+    var animated = 'animated';
+    box.each(function(){
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+      if(scrollPos > boxOffset - wh + 300 ){
+        $(this).addClass(animated);
+      }
+    });
+    var box = $('.five');
+    var animated = 'animated';
+    box.each(function(){
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+      if(scrollPos > boxOffset - wh + 300 ){
+        $(this).addClass(animated);
+      }
+    });
   });
+
+
+
+
