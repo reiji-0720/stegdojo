2d5d18d6a5aa5364bfd336751a8c923d4904f2d5
REiJi <shino327gh@gmail.com>
2019-11-29 02:50:21 +0900

    s

--- Lv8/lv8.js
+++ Lv8/lv8.js
@@ -4,10 +4,15 @@ var table = document.createElement('table');
 for ( var i = 1; i < 9; i++){
  
     var div = document.createElement('div');
-    div.className = 'box'+ i;
+    div.className = 'box';
+
+    var tmp = document.getElementsByClassName("box")
+    var val="images"+ i;
+    tmp[i-1].setAttribute("id",val)
+
 
     var img = document.createElement('img');
     img.src = './' + i +'.jpg';
     img.alt = i + 'の画像';
-    document.getElementById('test').appendChild(div)
+    document.getElementById('iamges'+ i).appendChild(div)
 };
