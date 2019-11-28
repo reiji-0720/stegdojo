7ab500c0cc2a13a676dbd1a93582c550af299708
REiJi <shino327gh@gmail.com>
2019-11-29 02:20:03 +0900

    s

--- Lv8/lv8.js
+++ Lv8/lv8.js
@@ -1,1 +1,13 @@
-test.insertAdjacentHTML('afterEnd','<div>afterEnd</div>')
+
+var table = document.createElement('table');
+
+for ( var i = 1; i < 11; i++){
+
+    var div = document.createElement('div');
+    div.className = 'box'+ i;
+
+    var img = document.createElement('img');
+    img.src = './' + i +'.jpg';
+    img.alt = i + 'の画像';
+    document.getElementById('test').appendChild(div)
+};
